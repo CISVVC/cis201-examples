dda9df0d3da5f64e8c595a9d42ddbd4d090f7845
Paul Tonning <paul.tonning@student.vvc.edu>
2019-11-14 19:59:55 -0800

    update

--- pointer-structures/sandbox/strings/main.cpp
+++ pointer-structures/sandbox/strings/main.cpp
@@ -32,27 +32,28 @@ int main()
     const char *c = input.c_str();
     const char **line_array = new const char*[lines];
     line_array[0] = c;
-    print_string(line_array[0]);
     while(*c != '\n')
        c++;
     c++;
     line_array[1] = c;
-    print_string(line_array[1]);
     while(*c != '\n')
        c++;
     c++;
     line_array[2] = c;
-    print_string(line_array[2]);
     while(*c != '\n')
        c++;
     c++;
     line_array[3] = c;
-    print_string(line_array[3]);
     while(*c != '\n')
        c++;
     c++;
     line_array[4] = c;
+
     print_string(line_array[4]);
+    print_string(line_array[3]);
+    print_string(line_array[2]);
+    print_string(line_array[1]);
+    print_string(line_array[0]);
 
     return 0;
 }
