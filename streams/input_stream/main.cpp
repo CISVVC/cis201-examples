10cfd75451e24908e9462e0240f459bcf7568f78
Paul Tonning <paul.tonning@student.vvc.edu>
2019-11-19 20:26:25 -0800

    update

--- streams/input_stream/main.cpp
+++ streams/input_stream/main.cpp
@@ -1,25 +1,28 @@
 #include<iostream>
 #include<string>
 #include<fstream>
+#include<vector>
 
 using namespace std;
 
 int main()
 {
-   ifstream input;
-   string name;
-   cout << "Enter the filename: ";
-   cin >> name;
-   input.open(name.c_str());
-   
-   char ch;
-   while(input.get(ch))
-   {
-      cout << ch;
-   }
-   cout << endl;
 
-   input.close();
+   ifstream input;
+
+   input.open("/usr/share/dict/words");
+
+   string word;
+   vector<string> word_list;
+
+   while(input >> word)
+   {
+      word_list.push_back(word);
+   }
+
+   for(string w : word_list)
+      cout << w << endl;
 
    return 0;
+
 }
